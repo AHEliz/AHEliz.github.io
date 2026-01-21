06e5b209848f9204112f66f7fd5bc34910f9a5a3
AHEliz <allisonhuayapa@gmail.com>
2026-01-20 23:20:56 -0500

    Update votaciones.js

--- data/votaciones.js
+++ data/votaciones.js
@@ -1,20 +1,24 @@
 const votaciones = [
   {
-    nombre: "Mubeat",
-    fecha: "2026-01-25",
-    link: "../voting/mubeat.html",
-    descripcion: "Votación diaria en la app Mubeat"
+    id: "fancast",
+    titulo: "33rd Anniversary HANTEO MUSIC AWARDS 2025",
+    plataforma: "FANCAST",
+    inicio: "2026-01-16",
+    fin: "2026-01-25",
+    fechaTexto: "25.01.2026",
+    imagen: "img/fancast.png",
+    linkVotacion: "https://t.co/wIvLksMmLa",
+    descripcion: "Votación Fancast [2nda Ronda]"
   },
   {
-    nombre: "Fancast",
-    fecha: "2026-01-25",
-    link: "../voting/fancast.html",
-    descripcion: "Votación en Fancast"
-  },
-  {
-    nombre: "MAMA",
-    fecha: "2026-01-31",
-    link: "../voting/mama.html",
-    descripcion: "Premios MAMA 2026"
+    id: "mubeat",
+    titulo: "Mubeat 2025",
+    plataforma: "MUBEAT",
+    inicio: "2026-01-16",
+    fin: "2026-01-25",
+    fechaTexto: "25.01.2026",
+    imagen: "img/mubeat.png",
+    linkVotacion: "https://t.co/rSqZBBCwhY",
+    descripcion: "[Round 2] GLOBAL POPULAR ARTIST."
   }
 ];
