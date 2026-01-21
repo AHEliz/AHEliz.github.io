31c8d403c929160df1ca4552e41296b967938fdb
AHEliz <allisonhuayapa@gmail.com>
2026-01-20 23:14:21 -0500

    Update votaciones.js

--- data/votaciones.js
+++ data/votaciones.js
@@ -1,1 +1,20 @@
-
+const votaciones = [
+  {
+    nombre: "Mubeat",
+    fecha: "2026-01-25",
+    link: "../voting/mubeat.html",
+    descripcion: "Votación diaria en la app Mubeat"
+  },
+  {
+    nombre: "Fancast",
+    fecha: "2026-01-25",
+    link: "../voting/fancast.html",
+    descripcion: "Votación en Fancast"
+  },
+  {
+    nombre: "MAMA",
+    fecha: "2026-01-31",
+    link: "../voting/mama.html",
+    descripcion: "Premios MAMA 2026"
+  }
+];
